21bd10cf3b616c262fa7cf3e042259fb1920b303
German Lena <german.lena@gmail.com>
2015-11-03 15:04:26 -0300

    fix for new format

--- js/reservaschart.js
+++ js/reservaschart.js
@@ -128,7 +128,7 @@ var chart = (function (options) {
 			var scope = this;
 			d3.json("data.json?r="+Math.random(), function(error, dataset) {
 
-				var monto = dataset.ultimo.monto;
+				var monto = dataset.historico[0].monto;
 				d3.select(scope.options.currentValueSelector).text(scope.formatNumber(monto));
 
 				scope.data = dataset;
